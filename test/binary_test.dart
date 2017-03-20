2f1a3c15118946823d7d4338d661edf34a577525
kharland <kendaljharland@gmail.com>
2017-03-19 21:58:04 -0700

    Use single test-case instead of group

--- test/binary_test.dart
+++ test/binary_test.dart
@@ -115,13 +115,13 @@ void main() {
     });
   });
 
-  group('arithmeticShiftRight should', () {
-    test('left-pad a logically right-shifted number with its msb', () {
-      // 1111 1111 -> 1111 1111
-      expect(uint8.arithmeticShiftRight(0xFF, 5), 0xFF);
-      // 0111 1111 -> 0000 0011
-      expect(uint8.arithmeticShiftRight(0x7F, 5), 0x03);
-    });
+  test(
+      'arithmeticShiftRight should left-pad a logically right-shifted number '
+      'with its msb', () {
+    // 1111 1111 -> 1111 1111
+    expect(uint8.arithmeticShiftRight(0xFF, 5), 0xFF);
+    // 0111 1111 -> 0000 0011
+    expect(uint8.arithmeticShiftRight(0x7F, 5), 0x03);
   });
 
   test('areSet should return the number of set bits', () {
